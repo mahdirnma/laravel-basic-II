c66bff84909dd8eb03f1a3b3024c97d8b3878f03
mahdi <rahnamamahdi78@gmail.com>
2024-05-05 01:09:57 +0330

    create db and models and controllers for project

--- delta-retaurant/database/seeders/DatabaseSeeder.php
+++ delta-retaurant/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Category;
+use App\Models\Food;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,9 +16,23 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+         Category::create([
+             'title' => 'pizza'
+         ]);
+         Category::create([
+             'title' => 'sandwich'
+         ]);
+         Food::create([
+             'title' => 'chicken',
+             'food_type' => 'normal',
+             'entity' => 'yes',
+             'category_id' => '2'
+         ]);
+         Food::create([
+             'title' => 'vegetable',
+             'food_type' => 'vegan',
+             'entity' => 'no',
+             'category_id' => '1'
+         ]);
     }
 }
