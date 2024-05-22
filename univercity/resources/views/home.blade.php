d22a19e9d064714d246bfff23f1918d2e54a8553
mahdi <rahnamamahdi78@gmail.com>
2024-05-22 23:44:28 +0330

    add Tailwind to project and start design home page

---
+++ univercity/resources/views/home.blade.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+<div class="bg-gray-200 w-full h-screen flex justify-center items-center">
+    <div class="bg-white w-11/12 h-5/6 rounded-2xl flex">
+        <div class="w-2/12 h-full bg-violet-950 rounded-s-3xl flex-col items-center py-7">
+            <h1 class="text-white flex justify-center text-2xl font-medium">UNIVERSITY</h1>
+            <div class="w-full h-px bg-gray-200 my-7"></div>
+            <nav class="h-1/2 w-full px-7 flex items-center">
+                <p class="text-white">Main</p>
+                <ul>
+                    <li><a href=""></a></li>
+                    <li><a href="">students</a></li>
+                    <li><a href="">professors</a></li>
+                    <li><a href="">lessons</a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</div>
+</body>
+</html>
