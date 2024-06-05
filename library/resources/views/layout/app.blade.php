4fff94091e8bc004c71d63864679f15a240b60c8
mahdi <rahnamamahdi78@gmail.com>
2024-06-05 23:57:16 +0330

    design navigation of app.blade

--- library/resources/views/layout/app.blade.php
+++ library/resources/views/layout/app.blade.php
@@ -10,11 +10,25 @@
 </head>
 <body>
 <div class="w-svw h-svh">
-    <div class="w-full h-24 flex items-center pl-16">
+    <div class="w-full h-[13%] flex items-center pl-16">
         <h1 class="text-3xl font-bold text-blue-500">Library</h1>
-        <form action="" method="get" class="w-7/12 h-full flex items-end justify-center pb-3">
-            <input type="search" name="search" id="search" placeholder="search" class="outline-0 pl-10 rounded-full w-8/12 h-14 bg-gray-100">
+        <form action="" method="get" class="w-7/12 h-full flex items-end justify-end pb-3">
+            <label for="search"></label>
+            <input type="search" name="search" id="search" placeholder="search" class="outline-0 pl-10 rounded-full w-9/12 h-14 bg-gray-100">
         </form>
+    </div>
+    <div class="w-full h-[87%] flex">
+        <div class="w-2/12 h-full rounded-tr-[70px] bg-blue-500">
+            <nav class="w-full h-4/6">
+                <ul class="w-full h-full flex flex-col items-end justify-center">
+                    <li class="w-10/12 text-start text-white font-medium tracking-wide my-2"><a href="">dashboard</a></li>
+                    <li class="w-10/12 text-start text-white font-medium tracking-wide my-2"><a href="">books</a></li>
+                    <li class="w-10/12 text-start text-white font-medium tracking-wide my-2"><a href="">writers</a></li>
+                    <li class="w-10/12 text-start text-white font-medium tracking-wide my-2"><a href="">trusts</a></li>
+                    <li class="w-10/12 text-start text-white font-medium tracking-wide my-2"><a href="">categories</a></li>
+                </ul>
+            </nav>
+        </div>
     </div>
 </div>
 </body>
